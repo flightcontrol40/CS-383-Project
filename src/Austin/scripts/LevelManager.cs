b26ac9d4689656d38f061a34280d26065ec030d9
Austin Walker <ajwblueidaho@gmail.com>
2024-10-22 22:53:34 -0700

    Made some structural changes and added some functions/signals to get ready for integration with other features

--- src/Austin/scripts/LevelManager.cs
+++ src/Austin/scripts/LevelManager.cs
@@ -6,83 +6,55 @@ using System.Linq;
 public partial class LevelManager : Node
 {
     [Export]
-    private Level level;
+    public Level level;
+
+    private Map currentMap;
+
     [Export]
-    public Map currentMap;
+    private bool levelLoaded = false;
 
-    private RoundManager.RoundManager roundManager;
-
-	[Export]
-	private bool LevelLoaded = false;
+    [Signal]
+    public delegate void LoadRoundEventHandler(DifficultyTable difficulty);
 
     public override void _Ready()
     {
-        this.roundManager = this.GetNode<RoundManager.RoundManager>("RoundManager");
-        base._Ready();
+        return;
     }
-
 
     public override void _Process(double delta)
     {
         // this was used to test if the map would load
         if (Input.IsActionJustPressed("load_map")) {
-            loadMap();
+            //load the map
+            currentMap = level.loadMap();
+            //book keeping
+            if (currentMap != null) {
+                AddChild(currentMap);      //makes visible on screen
+                currentMap.SetOwner(this); //makes visible in scene tree, and able to be packed into PackedScene
+            }
         } else
         if (Input.IsActionJustPressed("unload_map")) {
-            unloadMap();
-        }
-        if (Input.IsActionJustPressed("start_round")) {
-            startRound();
+            level.unloadMap();
         }
     }
 
-    private void startRound(){
-        /*
-        if (!IsInstanceValid(this.roundManager)){
-            this.loadMap();
+    void OnLoadLevel() {
+        // load the map
+        currentMap = level.loadMap();
+        //book keeping
+        if (currentMap != null) {
+            AddChild(currentMap); //makes map visible on the screen
+            currentMap.SetOwner(this); // makes visible in scene tree, and able to be serialized into a PackedScene
         }
-        this.roundManager.startRound();
-        */
-    }
-
-    // Loads a map and add's its node as a child of the LevelManager
-    // Note: it won't load a map if on is already loaded
-    public void loadMap() { 
-        if (!IsInstanceValid(currentMap)) {
-            currentMap = (Map)level.mapScene.Instantiate();
-            AddChild(currentMap);
-        }
-    }
-    
-    // Unloads a map if one has been loaded
-    public void unloadMap() {
-        if (IsInstanceValid(currentMap)) {
-            currentMap.QueueFree();
-        }
-    }
-
-
-
-    void OnLoadLevel(RoundManager.RoundManager roundManager) {
-
-    }
-
-    // Probably adds a tower to the tower record
-    public void addTower(Node2D tower) {
-        level.towers.Append(tower);
-    }
-
-    // Probably removes a tower from tower records
-    public void removeTower(Node2D tower) {
-        level.towers = level.towers.Where(n => n != tower).ToArray();
-        tower.QueueFree();
+        // load round
+        EmitSignal(SignalName.LoadRound, level.difficultyTable);
     }
 
     Path2D getPath() {
         return currentMap.GetNode<Path>("Path").getPath();
     }
 
-    public int Health { 
+    public int playerHealth { 
         get { return this.level.playerHealth; }
         set { this.level.playerHealth = Math.Max(value, 0); }
     }
@@ -92,16 +64,58 @@ public partial class LevelManager : Node
         set { this.level.currentRoundNum = Math.Clamp(value, 0, level.MaxRound); }
     }
 
-    public Path2D LevelPath {
-        get { return currentMap.GetNode<Path>("Path").GetNode<Path2D>("Path2D"); }
+    public void setMap(PackedScene mapScene) {
+        if (!levelLoaded) {
+            level.mapScene = mapScene;
+        }
     }
 
-    public void setLevel(Level newLevel) {
-        level = newLevel;
+    public void setDifficutly(Difficulty difficulty) {
+        if (!levelLoaded) {
+            level.baseDifficulty = difficulty;
+
+            DifficultyTable newDifficultyTable = (DifficultyTable)GD.Load<Resource>("res://src/Nathan/DifficultyTable.cs");
+            switch (difficulty) {
+                case Difficulty.Easy:
+                    loadEasyDifficultyTable(newDifficultyTable);
+                    break;
+                case Difficulty.Medium:
+                    loadMediumDifficultyTable(newDifficultyTable);
+                    break;
+                case Difficulty.Hard:
+                    loadHardDifficultyTable(newDifficultyTable);
+                    break;
+                default:
+                    loadEasyDifficultyTable(newDifficultyTable);
+                    break;
+            }
+
+            level.difficultyTable = newDifficultyTable;
+        }
     }
 
-    public bool isMapLoaded() {
-        return IsInstanceValid(currentMap);
+    public bool mapLoaded {
+        get { return IsInstanceValid(currentMap); }
+    }
+
+    private void loadEasyDifficultyTable(DifficultyTable difficultyTable) {
+        // figure out EnemyRanks, should they be defined by chickens?
+
+        difficultyTable.RoundDifficultyValue = new int[level.MaxRound];
+
+
+        for (int i = 0; i < level.MaxRound; i++) {
+            difficultyTable.RoundDifficultyValue[i] = 100; // set this to an exponential equation to gradually bring up the difficulty
+        }
+    }
+
+
+    private void loadMediumDifficultyTable(DifficultyTable difficultyTable) {
+        
+    }
+
+    private void loadHardDifficultyTable(DifficultyTable difficultyTable) {
+        
     }
 
 }
