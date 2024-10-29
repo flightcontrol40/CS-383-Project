2d1d7d024f2eee5c96d90a64772de57b41c8fe8c
Nathan Hampton <nathanhampton40000@gmail.com>
2024-10-29 10:57:24 -0700

    Updated Stress Test

---
+++ src/Nathan/Tests/NathanSampleScene.cs
@@ -0,0 +1,42 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+using RoundManager;
+
+
+public partial class NathanSampleScene : Node2D
+{
+    DifficultyTable difficultyTable = GD.Load<DifficultyTable>("res://src/Nathan/Tests/SampleHardTable.tres");
+    RoundManager.RoundManager roundManager;
+    RoundManager.DifficultyCalculator difficultyCalculator;
+    List<SpawnOrder> spawnOrders;
+    
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready(){
+        this.roundManager = this.GetNode<RoundManager.RoundManager>("RoundManager");
+        this.difficultyCalculator = DifficultyCalculatorFactory.CreateCalculator(
+            this.difficultyTable,
+            Difficulty.Hard
+        );
+        this.AddChild(difficultyCalculator);
+        this.spawnOrders = new();
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta) {
+    }
+
+    public int spawn_round(int roundNumber){
+        List<SpawnOrder> orders = this.difficultyCalculator.CalculateSpawnOrder(roundNumber);
+        foreach (SpawnOrder order in orders){
+            this.spawnOrders.Add(order);
+            // Prevent Memory Leak
+            if (order.GetParent() == null){
+                this.AddChild(order);
+            }
+        }
+        return this.spawnOrders.Count;
+    }
+
+}
