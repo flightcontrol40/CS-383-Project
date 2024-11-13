10065378c03f3c3af00e1ac3dcba16b8a3e2b169
Clayton <nvxharpy@gmail.com>
2024-11-13 00:45:26 -0800

    Most test cases, could not for the life of me figure out my stress tho. It is far too late for this shit and I will get after it tommorow

--- src/Clayton/test/FrankTest.cs
+++ src/Clayton/test/FrankTest.cs
@@ -5,18 +5,15 @@ using Chicken;
 using static GdUnit4.Assertions;
 
 namespace ClaytonTest {
-	
 	[TestSuite]
 	public partial class FrankTests : Node
 	{
-		
 		public BaseChicken chicken2;
 		[Before] //load in chickens
 		public void ChickenLoader2() {
 			//load the Frank chicken
 			chicken2 = GD.Load<PackedScene>("res://src/Clayton/Enemy/Frank.tscn").Instantiate<Frank>();
 		}
-		
 		[TestCase]
 		public void HealthCheck2() {
 			int x = 300;
@@ -29,21 +26,40 @@ namespace ClaytonTest {
 			int y = chicken2.damageAmount;
 			AssertThat(y).IsEqual(x);
 		}
-		
 		[TestCase]
 		public void SpeedCheck2() {
 			int x = 200;
 			int y = chicken2.Speed;
 			AssertThat(y).IsEqual(x);
 		}
-		
-		
-		
+		[TestCase]
+		public void RankCheck2() {
+			int x = 2;
+			int y = chicken2.EnemyRank;
+			AssertThat(y).IsEqual(x);
+		}
+		[TestCase]
+		public void StartedCheck2() {
+			bool check = false;
+			bool chick = chicken2.started;
+			AssertThat(chick).IsEqual(check);
+		}
+		[TestCase]
+		public void VisibleTest2() {
+			chicken2._Ready();
+			bool VCheck = false;
+			bool VChick = chicken2.Visible;
+			AssertThat(VChick).IsEqual(VCheck);			
+		}		
+		[TestCase]
+		public void DamageTest2(){
+			int NHealth = chicken2.Health;
+			chicken2.TakeDamage(50);
+			AssertThat(chicken2.Health).IsEqual(NHealth-50);
+		}
 		[After]
 		public void ChickenDeLoader2(){
 			chicken2.Free();
-			
 		}
-	
 	}
 }
--- src/Clayton/test/FrankestTest.cs
+++ src/Clayton/test/FrankestTest.cs
@@ -5,18 +5,15 @@ using Chicken;
 using static GdUnit4.Assertions;
 
 namespace ClaytonTest {
-	
 	[TestSuite]
 	public partial class FrankestTests : Node
 	{
-		
 		public BaseChicken chicken3;
 		[Before] //load in chickens
 		public void ChickenLoader3() {
 			//load the Frankest chicken
 			chicken3 = GD.Load<PackedScene>("res://src/Clayton/Enemy/Frankest.tscn").Instantiate<Frankest>();
 		}
-		
 		[TestCase]
 		public void HealthCheck3() {
 			int x = 900;
@@ -29,21 +26,40 @@ namespace ClaytonTest {
 			int y = chicken3.damageAmount;
 			AssertThat(y).IsEqual(x);
 		}
-		
 		[TestCase]
 		public void SpeedCheck3() {
 			int x = 250;
 			int y = chicken3.Speed;
 			AssertThat(y).IsEqual(x);
 		}
-		
-		
-		
+		[TestCase]
+		public void RankCheck3() {
+			int x = 3;
+			int y = chicken3.EnemyRank;
+			AssertThat(y).IsEqual(x);
+		}		
+		[TestCase]
+		public void StartedCheck3() {
+			bool check = false;
+			bool chick = chicken3.started;
+			AssertThat(chick).IsEqual(check);
+		}
+		[TestCase]
+		public void VisibleTest3() {
+			chicken3._Ready();
+			bool VCheck = false;
+			bool VChick = chicken3.Visible;
+			AssertThat(VChick).IsEqual(VCheck);			
+		}
+		[TestCase]
+		public void DamageTest3(){
+			int NHealth = chicken3.Health;
+			chicken3.TakeDamage(50);
+			AssertThat(chicken3.Health).IsEqual(NHealth-50);
+		}
 		[After]
 		public void ChickenDeLoader3(){
 			chicken3.Free();
-			
 		}
-	
 	}
 }
