ef70c131fd8323cfdb47dba7ea26b5c89f7bae17
Austin Walker <ajwblueidaho@outlook.com>
2024-11-18 21:42:40 -0800

    Commented out the level_converter to essentially get rid of it

--- src/Austin/scripts/level_converter.cs
+++ src/Austin/scripts/level_converter.cs
@@ -1,66 +1,63 @@
-
-
+/*
 using DifficultyCalculator;
 using RoundManager;
 using Godot;
 using Castle.Components.DictionaryAdapter;
 
-namespace LevelConverter {
+public abstract class LevelConverter {
+    protected Level level;
 
-    public abstract class LevelConverter {
-        protected Level level;
-
-        public LevelConverter() {
-            level = GD.Load<Level>("res://src/Austin/scripts/custom_resources/Level.cs");
-        }
-
-        public LevelConverter(Level levelData) {
-            level = levelData;
-        }
-
-        public abstract void setMap(PackedScene mapScene);
-
-        public abstract void setDifficulty(Difficulty difficulty);
-
-        public Level getLevel() {
-            return level;
-        }
+    public LevelConverter() {
+        level = GD.Load<Level>("res://src/Austin/scripts/custom_resources/Level.cs");
     }
 
-    public class SavedResourceConverter : LevelConverter {
-        string resourcePath = "";
-
-        public SavedResourceConverter(string path, Level level) {
-            resourcePath = path;
-            this.level = level;
-        }
-
-        public SavedResourceConverter(string path) {
-            resourcePath = path;
-
-            level = new Level();
-        }
-
-        public override void setDifficulty(Difficulty difficulty) {
-            level.setDifficulty(difficulty);
-        }
-
-        public override void setMap(PackedScene mapScene) {
-            if (mapScene != null) {
-                level.mapScene = mapScene;
-            }
-        }
+    public LevelConverter(Level levelData) {
+        level = levelData;
     }
 
-    public class ResourceInstanceConverter : LevelConverter {
-        public override void setDifficulty(Difficulty difficulty) {
-            level.setDifficulty(difficulty);
-        }
+    public abstract void setMap(PackedScene mapScene);
 
-        public override void setMap(PackedScene mapScene) {
-            if (mapScene != null) {
-                level.mapScene = mapScene;
-            }
+    public abstract void setDifficulty(Difficulty difficulty);
+
+    public Level getLevel() {
+        return level;
+    }
+}
+
+public class SavedResourceConverter : LevelConverter {
+    string resourcePath = "";
+
+    public SavedResourceConverter(string path, Level level) {
+        resourcePath = path;
+        this.level = level;
+    }
+
+    public SavedResourceConverter(string path) {
+        resourcePath = path;
+
+        level = new Level();
+    }
+
+    public override void setDifficulty(Difficulty difficulty) {
+        level.setDifficulty(difficulty);
+    }
+
+    public override void setMap(PackedScene mapScene) {
+        if (mapScene != null) {
+            level.mapScene = mapScene;
         }
     }
 }
+
+public class ResourceInstanceConverter : LevelConverter {
+    public override void setDifficulty(Difficulty difficulty) {
+        level.setDifficulty(difficulty);
+    }
+
+    public override void setMap(PackedScene mapScene) {
+        if (mapScene != null) {
+            level.mapScene = mapScene;
+        }
+    }
+}
+*/
