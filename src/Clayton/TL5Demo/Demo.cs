cc5186048e63059a01e30e0e8bfe41976280126f
Clayton <nvxharpy@gmail.com>
2024-12-03 23:08:39 -0800

    Demo mode

---
+++ src/Clayton/TL5Demo/Demo.cs
@@ -0,0 +1,20 @@
+using Godot;
+
+public partial class DemoMenu : Control
+{
+	private Button returnButton; // Button for returning to the main menu
+
+	public override void _Ready()
+	{
+		returnButton = GetNode<Button>("VBoxContainer/Button"); 
+		returnButton.Pressed += OnReturnButtonPressed;
+	}
+
+	private void OnReturnButtonPressed()
+	{
+		GD.Print("working");
+		GetTree().ChangeSceneToFile("res://src/Austin/scenes/main.tscn");
+		//var mainMenu = GetNode<MainMenu>("res://src/Sohan/Scenes/main_menu.tscn"); 
+		//mainMenu.EmitSignal(nameof(MainMenu.ReshowMenuEventHandler)); // Emit the signal to show the main menu
+	}
+}
